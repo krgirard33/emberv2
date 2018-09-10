bce963a6f676a93e9264336416f181e6a0e50764
Ken Girard <ken.girard@gmail.com>
2018-09-09 23:03:04 -0700

    Started adding in a TicketType

--- src/main/java/com/nosin/emberv2/model/data/AttendeeDao.java
+++ src/main/java/com/nosin/emberv2/model/data/AttendeeDao.java
@@ -1,9 +1,11 @@
 package com.nosin.emberv2.model.data;
 
 import com.nosin.emberv2.model.Attendee;
+import com.nosin.emberv2.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
+import java.util.List;
 
 
 @Repository
