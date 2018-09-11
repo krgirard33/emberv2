02790083974ad6dfca92a4690cf0276440ce981d
Ken Girard <ken.girard@gmail.com>
2018-09-11 03:06:56 -0500

    Uncommented MvcConfig, got rid of an empty line in WebSecurityConfig

--- src/main/java/com/nosin/emberv2/config/MvcConfig.java
+++ src/main/java/com/nosin/emberv2/config/MvcConfig.java
@@ -1,6 +1,6 @@
 package com.nosin.emberv2.config;
 
-/*
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,5 +16,5 @@ public class MvcConfig implements WebMvcConfigurer {
     }
 
 }
-*/
+
 
